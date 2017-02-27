c3a7f648c86633d5c019ac513ecd2034fba426f4
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-27 22:52:39 +0530

    Added addGreeting function

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 import './HelloWorldList.css';
 
 class HelloWorldList extends Component {
     constructor(props) {
         super(props);
         this.state = { greetings: ['Mayank', 'Maanas', 'Smriti'] };
+        this.addGreeting = this.addGreeting.bind(this);
     }
 
     // Return the render component based
@@ -18,9 +20,14 @@ class HelloWorldList extends Component {
         ));
     }
 
+    addGreeting(newName) {
+        this.setState({ greetings: [...this.state.greetings, newName] });
+    }
+
     render() {
         return (
             <div className="HelloWorldList">
+                <AddGreeter addGreeting={this.addGreeting}/>
                 {this.renderGreetings()}
             </div>
         );
