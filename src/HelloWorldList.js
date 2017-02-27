3c97e12f2324bb007aedad4c57415c3197728fbc
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-27 21:53:27 +0530

    Rendering HelloWorldList component dynamically

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -3,11 +3,25 @@ import HelloWorld from './HelloWorld';
 import './HelloWorldList.css';
 
 class HelloWorldList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { greetings: ['Mayank', 'Maanas', 'Smriti'] };
+    }
+
+    // Return the render component based
+    // on state
+    renderGreetings() {
+        // Use index of element in state as key
+        // as there is no unique key available now
+        return this.state.greetings.map((name, ind) => (
+            <HelloWorld key={ind} name={name}/>
+        ));
+    }
+
     render() {
         return (
             <div className="HelloWorldList">
-                <HelloWorld name="Mayank"/>
-                <HelloWorld name="Maanas"/>
+                {this.renderGreetings()}
             </div>
         );
     }
