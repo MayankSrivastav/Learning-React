d3fad5b34123889f6c35bf31362dbe0f7fca3f57
MayankSrivastav <mayanksri18@yahoo.in>
2017-02-27 19:32:43 +0530

    App.js now renders HelloWorldList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Learn from './Learn';
+import HelloWorldList from './HelloWorldList';
 // import './App.css';
 
 const App = () => {  
   return (
     <div className="App">
-      <Learn name="Mayank"/>
-      <Learn name="Maanas"/>
-    </div>);
+      <HelloWorldList/>
+    </div>
+  );
 };
 
 export default App;
